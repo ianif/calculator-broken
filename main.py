acd998a0dffa7265834b757a5dcba2981bdfc6f9
Ioanna Anyfanti <ioanna@turintech.ai>
2025-09-10 14:52:32 +0100

    Update main.py

--- main.py
+++ main.py
@@ -5,5 +5,5 @@ if __name__ == "__main__":
     print("1 + 2 =", add(1, 2))
     print("5 - 3 =", subtract(5, 3))
     print("4 * 7 =", multiply(4, 7))
-    print("10 / 2 =", divide(10, 2))
+    print("10 / 3 =", divide(10, 3)
 
